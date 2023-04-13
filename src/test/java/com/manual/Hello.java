db54445cb3dda57d36e4bef1c36ee3cf039ae2f0
arun <arunajin671@gmail.com>
2023-04-13 18:03:03 +0530

    first push

---
+++ src/test/java/com/manual/Hello.java
@@ -0,0 +1,21 @@
+package com.manual;
+
+public class Hello {
+	
+	
+	public void tamil() {
+		System.out.println("tamil");
+
+	}
+	
+	public void english() {
+		System.out.println("english");
+
+	}
+	
+	public void telugu() {
+		System.out.println("telugu");
+
+	}
+
+}
